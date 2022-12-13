9d8c6fa765e0082031ea8ce0e75f6b2a805fa257
LOYI4o2ropo[ <e.maykov@r-top.ru>
2022-12-13 14:20:43 +0300

    first commi2dt

--- src/scripts/calculator.js
+++ src/scripts/calculator.js
@@ -1,3 +1,24 @@
+/* eslint-disable no-undef */
+"use strict";
+document.addEventListener("DOMContentLoaded", () => {
+  const sliderIntro = new Swiper(".intro", {
+    slidesPerView: 1,
+    loop: false,
+    navigation: {
+      nextEl: ".intro-next",
+      prevEl: ".intro-prev",
+    }
+  });
+  const sliderIntroCalculator = new Swiper(".intro-calculator", {
+    slidesPerView: 1,
+    loop: false,
+    navigation: {
+      nextEl: ".intro-next",
+      prevEl: ".intro-prev",
+    }
+  });
+});
+
 let outputHouse = document.querySelector('#rangeValueHouse');
 let outputRegion = document.querySelector('#rangeValueRegion');
 let widthWindow = document.documentElement.clientWidth;
